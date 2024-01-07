6cc9c82bec6f34173c58e9dd412a89ddd0d343e9
Michael Shi <61084115+Micsushi@users.noreply.github.com>
2024-01-07 08:18:51 -0700

    Functioning highlight search

--- src/background.js
+++ src/background.js
@@ -47,3 +47,24 @@ chrome.commands.onCommand.addListener(async (command) => {
     });
   }
 });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("message received in background.js: ", message);
+  if (message.type === 'HIGHLIGHTED_TEXT') {
+    fetch('http://localhost:3000/generate-text', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ highlightedText: message.text })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message);
+      sendResponse({ message: data.message }); // Send response back
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      sendResponse({ error: error.message });
+    });
+    return true; // Indicates you want to send a response asynchronously
+  }
+});
