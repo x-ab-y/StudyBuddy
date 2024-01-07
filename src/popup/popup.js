15986b92bef1dd9d3e33bad0df7c9ca4669979ed
Michael Shi <61084115+Micsushi@users.noreply.github.com>
2024-01-06 18:28:41 -0700

    Revert "Merge branch 'main' into feature-greyscale-mode"
    This reverts commit 427b9430583baae540cb7ee3430eb4f69257b491, reversing
    changes made to d86acc91a5010fae87e410f06c77c7a73f3bca63.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -15,7 +15,6 @@ let restoreButton = document.getElementById("restore-button");
 let highlightSheetInput = document.getElementById("highlight-input");
 let restSheetInput = document.getElementById("rest-input");
 let algorithmInput = document.getElementById("algorithmInput");
-let fontSelect = document.getElementById("font-select");
 
 var buttonEnabledClass = "button-enabled";
 var buttonDisabledClass = "button-disabled";
@@ -169,24 +168,6 @@ greyscale.addEventListener("click", async () => {
     chrome.storage.sync.set({ greyscale: !data.greyscale });
   });
 
-fontSelect.addEventListener('change', async function(event) {
-  var selectedFont = event.target.value;
-  // Save the selected font to chrome.storage
-  chrome.storage.sync.set({font: selectedFont}, function() {
-    console.log('Font saved as ' + selectedFont);
-  });
-
-  // Get the current active tab
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: bionify,
-  });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: bionify,
-  });
 });
 
 autoButton.addEventListener("click", async () => {
@@ -215,11 +196,3 @@ function onRestInputChange() {
 function onAlgorithmInputChange() {
   chrome.storage.sync.set({ algorithm: algorithmInput.value });
 }
-
-document.querySelector('#go-to-options').addEventListener('click', function() {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
-  } else {
-    window.open(chrome.runtime.getURL('options.html'));
-  }
-});
